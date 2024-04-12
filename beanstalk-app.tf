6667229dffcbfefc8a2f3a91b5b94449d37311bb
David Mastropolo <dave.mastropolo@gmail.com>
2024-04-12 10:09:17 -0700

    backend-services.tf beanstalk-app.tf beanstalk-env.tf

---
+++ beanstalk-app.tf
@@ -0,0 +1,6 @@
+# Elastic Beanstalk app
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elastic_beanstalk_application
+resource "aws_elastic_beanstalk_application" "vprofile-project16-elastic-beanstalk-prod-application" {
+  name        = "vprofile-project16-elastic-beanstalk-prod-application"
+  description = "vprofile-project16-elastic-beanstalk-app-description"
