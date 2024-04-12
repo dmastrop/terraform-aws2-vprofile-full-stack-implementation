858cf170b1a86477f9a6ff1843d3f96a5c018ccb
David Mastropolo <dave.mastropolo@gmail.com>
2024-04-12 10:47:40 -0700

    formatting

--- beanstalk-env.tf
+++ beanstalk-env.tf
@@ -8,7 +8,7 @@
 resource "aws_elastic_beanstalk_environment" "vprofile-project16-elastic-beanstalk-prod-env" {
   name        = "vprofile-project16-elastic-beanstalk-prod-env"
   application = aws_elastic_beanstalk_application.vprofile-project16-elastic-beanstalk-prod-application
-  
+
   # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts.platforms.html
   # tomcat deployment::
   # https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.java
@@ -16,13 +16,13 @@ resource "aws_elastic_beanstalk_environment" "vprofile-project16-elastic-beansta
   #solution_stack_name = "64bit Amazon Linux 2 v4.1.1 running Tomcat 8.5 Correto 11"
   solution_stack_name = "64bit Amazon Linux 2023 v5.1.5 running Tomcat 9 Corretto 11"
   # the cname_prefix is the prepend to the URL
-  cname_prefix        = "vprofile-project16-bean-prod-domain"
+  cname_prefix = "vprofile-project16-bean-prod-domain"
 
 
-# the next blocks are a series of settings based upon the Option settings:
-# Option Settings
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elastic_beanstalk_environment#option-settings
-# https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
+  # the next blocks are a series of settings based upon the Option settings:
+  # Option Settings
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elastic_beanstalk_environment#option-settings
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
 
   setting {
     # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html#command-options-general-ec2vpc
@@ -46,11 +46,11 @@ resource "aws_elastic_beanstalk_environment" "vprofile-project16-elastic-beansta
   }
 
   setting {
-      # private subnet space for tomcat instances
+    # private subnet space for tomcat instances
     namespace = "aws:ec2:vpc"
     name      = "Subnets"
     # The IDs of the Auto Scaling group subnet or subnets. If you have multiple subnets, specify the value as a single comma-separated string of subnet ID
-    value = join(",", [module.vpc.private_subnets[0],module.vpc.private_subnets[1],module.vpc.private_subnets[2]])
+    value = join(",", [module.vpc.private_subnets[0], module.vpc.private_subnets[1], module.vpc.private_subnets[2]])
     # value is a string not a list, but join will convert the list to a string so that we can set the value as such.
     # https://developer.hashicorp.com/terraform/language/functions/join
   }
@@ -60,7 +60,7 @@ resource "aws_elastic_beanstalk_environment" "vprofile-project16-elastic-beansta
     namespace = "aws:ec2:vpc"
     name      = "ELBSubnets"
     # The IDs of the subnet or subnets for the elastic load balancer. If you have multiple subnets, specify the value as a single comma-separated string of subnet IDs
-    value = join(",", [module.vpc.public_subnets[0],module.vpc.public_subnets[1],module.vpc.public_subnets[2]])
+    value = join(",", [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]])
   }
 
   setting {
@@ -182,7 +182,7 @@ resource "aws_elastic_beanstalk_environment" "vprofile-project16-elastic-beansta
   # sometimes the security groups are not created before beanstalk environment
   # this is a bug that can occur via timing. Most of the time it will figure this out but best to create dependency
   # use depends_on 
-  depends_on = [aws_security_group.vprofile-project16-bean-elb-sg,aws_security_group.vprofile-project16-backend-sg,aws_security_group.vprofile-project16-prod-beanstalk-sg,aws_security_group.vprofile-project16-bastion-sg]
+  depends_on = [aws_security_group.vprofile-project16-bean-elb-sg, aws_security_group.vprofile-project16-backend-sg, aws_security_group.vprofile-project16-prod-beanstalk-sg, aws_security_group.vprofile-project16-bastion-sg]
 }
 
 
