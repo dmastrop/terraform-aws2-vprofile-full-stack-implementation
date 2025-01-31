8f6a37a5b96a461966c280823c8b041794c42e75
David Mastropolo <dave.mastropolo@gmail.com>
2025-01-30 17:45:38 -0800

    backend.tf add back in the 5.44.0 for aws provider. 5.84.0 is failing the terraform plan

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = "~> 5.25.0"
-  #   }
-  # }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.44.0"
+    }
+  }
   backend "s3" {
     bucket = "terraform-state-project16-vprofile"
     # this will create state in a folder called terraformstate/backend in the S3 bucket above
