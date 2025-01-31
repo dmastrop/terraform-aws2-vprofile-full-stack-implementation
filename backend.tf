e45044f53094572a06b9bbfacc11476e9ba89d71
David Mastropolo <dave.mastropolo@gmail.com>
2025-01-30 17:21:14 -0800

    backend.tf add 5.25.0 for version of aws provider

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.25.0"
     }
+  }
   backend "s3" {
     bucket = "terraform-state-project16-vprofile"
     # this will create state in a folder called terraformstate/backend in the S3 bucket above
